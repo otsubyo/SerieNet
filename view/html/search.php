2b41c0e178d3efda0f1df6b2914f503a57121f3c
Carl <carlos.premi@gmail.com>
2023-12-06 11:23:25 +0100

    script.js

--- view/html/search.php
+++ view/html/search.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <script src="../js/script.js"></script>
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="../css/base_style.css">
     <title>Accueil</title>
@@ -59,6 +59,10 @@
 <script src="../js/script.js"></script>
 </html>
 
+
+
+
 <section class="search-results">
     <div class="banner-content">
     </div>
+</section>
